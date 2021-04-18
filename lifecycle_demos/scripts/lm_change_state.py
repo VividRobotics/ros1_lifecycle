7322b6fe687cf6cc9169543ae9ad51befaf2ca13
Lucas Walter <wsacul@gmail.com>
2021-04-18 15:56:11 -0700

    Have a demo lifecycle managed node, and a command line client, but it isn't moving through states as expected, maybe it is getting locked up, errors and exceptions aren't getting out
    Now have working ability to change state from command line (TBD though two other scripts are in other packages here that do that, make one the only one).  Only issue is that finalized state skips itermediate states- maybe that was design choice, but would rather it deactivate and cleanup
    
    more convenient ways to change state, also eliminate loginfo from __del__ which was causing exceptions
    
    demonstrate having a parent lifecycle node manage children in sequence, reverse the sequence when deactivating and shutting down.  Add convenience timeout function with go_to_stated_timed but should revisit that

---
+++ lifecycle_demos/scripts/lm_change_state.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Copyright (c) 2021 Lucas Walter
+# Lucas Walter
+
+# Move a lifecycle node to a different state, then exit
+# TODO(lucasw) move this to a lifecycle utility package
+
+import rospy
+
+from lifecycle.client import create_client
+from lifecycle.lifecycle_model import LifecycleModel
+
+
+class LmChangeState(object):
+    def __init__(self):
+        # the namespace define which lifecycle node to connect to
+        self._client = create_client()
+
+        cur = rospy.Time.now()
+        while True:
+            elapsed = rospy.Time.now() - cur
+            if elapsed > rospy.Duration(4.0):
+                rospy.logerr("Couldn't get server state")
+                rospy.signal_shutdown("Couldn't get server state, exiting")
+                return
+            cur_state = self._client._server_state
+            if cur_state is not None:
+                break
+            rospy.sleep(0.2)
+
+        cur_state_str = LifecycleModel.STATE_TO_STR[cur_state]
+        rospy.loginfo("Cur state {} {} {}".format(rospy.get_namespace(), cur_state_str, cur_state))
+
+        goal_str = rospy.get_param("~goal", "inactive")
+        self.goal_state = LifecycleModel.STR_TO_STATE[goal_str]
+        rospy.loginfo("Goal state {} {}".format(goal_str, self.goal_state))
+        self._client.go_to_state(self.goal_state, self.transition_cb)
+
+    def transition_cb(self, result):
+        if result:
+            rospy.loginfo("result {}".format(result))
+        else:
+            rospy.logwarn("result {}".format(result))
+        rospy.signal_shutdown("finished state change attempt, exiting")
+
+
+if __name__ == '__main__':
+    rospy.init_node("lm_change_state")
+    lm_change_state = LmChangeState()
+    rospy.spin()
--- lifecycle_python/src/lifecycle/client.py
+++ lifecycle_python/src/lifecycle/client.py
@@ -130,8 +130,25 @@ class LifecycleClient(object):
         :return:
         """
         self._client = client
-        self._server_state = LifecycleGoal.PSTATE_UNCONFIGURED
+        self._server_state = None  # LifecycleGoal.PSTATE_UNCONFIGURED
         self._handle = None
+
+    # TODO(lucasw) not sure about having this pair here, but convenient for now
+    def completion_cb(self, result):
+        self._state_achieved = result
+
+    def go_to_state_timed(self, target_state, timeout=2.0):
+        self._state_achieved = None
+        self.go_to_state(target_state, self.completion_cb)
+        cur = rospy.Time.now()
+        # TODO(lucasw) how to properly await the callback?
+        while True:
+            rospy.sleep(0.1)
+            if self._state_achieved is not None:
+                return self._state_achieved
+            elapsed = rospy.Time.now() - cur
+            if elapsed.to_sec() > timeout:
+                return False
 
     def go_to_state(self, target_state, completion_cb):
         """
@@ -143,7 +160,11 @@ class LifecycleClient(object):
         """
         self.completion_cb_ = completion_cb
 
-        # if we're already in the target state, do nothing
+        if self._server_state is None:
+            rospy.logerr("Haven't received server state yet")
+            completion_cb(False)
+            return
+
         if target_state == self._server_state:
             rospy.loginfo("already in the target state, do nothing {}".format(target_state))
             completion_cb(True)
@@ -171,6 +192,8 @@ class LifecycleClient(object):
         :type msg: Lifecycle
         :return:
         """
+        if self._server_state is None:
+            rospy.loginfo("first state {}".format(msg.end_state))
         self._server_state = msg.end_state
 
     def _transition_completion_cb(self, result):
@@ -178,10 +201,19 @@ class LifecycleClient(object):
         self.completion_cb_(result)
 
 
-def create_client(component_fqn):
-    action_client = actionlib.action_client.ActionClient(component_fqn + "/" + LIFECYCLE_ACTION_NAME,
-                                                  LifecycleAction)
-    action_client.wait_for_server(timeout=rospy.Duration(1.0))
+def create_client(component_fqn=None):
+    if component_fqn != None and component_fqn != "":
+        component_fqn += "/"
+    else:
+        component_fqn = ""
+    action_path = component_fqn + LIFECYCLE_ACTION_NAME
+    state_path = component_fqn + LIFECYCLE_STATE_TOPIC
+    rospy.loginfo("{} {} {}".format(rospy.get_namespace(), action_path, state_path))
+
+    action_client = actionlib.action_client.ActionClient(action_path, LifecycleAction)
+    rv = action_client.wait_for_server(timeout=rospy.Duration(1.0))
+    rospy.loginfo("Connected to action client {}".format(rv))
     lc_client = LifecycleClient(action_client)
-    rospy.Subscriber(component_fqn + "/" + LIFECYCLE_STATE_TOPIC, Lifecycle, lc_client.state_cb)
+
+    rospy.Subscriber(state_path, Lifecycle, lc_client.state_cb)
     return lc_client
